73f1ff6f5d0bbd909c5538f30e397213ea6663fd
ssem-i <ssem2i@naver.com>
2025-04-18 23:36:03 +0900

    add spring form functionality

--- src/main/java/kr/ac/hansung/cse/controller/OfferController.java
+++ src/main/java/kr/ac/hansung/cse/controller/OfferController.java
@@ -2,6 +2,7 @@ package kr.ac.hansung.cse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 //import jakarta.validation.Valid;
+import jakarta.validation.Valid;
 import kr.ac.hansung.cse.model.Offer;
 import kr.ac.hansung.cse.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,27 @@ public class OfferController {
         model.addAttribute("id_offers", offers);
 
         return "offers";
+    }
+
+    @GetMapping("/createoffer")
+    public String createOffer(Model model){
+        model.addAttribute("offer", new Offer());
+        return "createoffer";  // 뷰로 넘겨줌
+    }
+    @PostMapping("docreate")
+    public String doCreate (Model model, @Valid Offer offer, BindingResult result) { // 데이터 바인딩
+        //System.out.println(offer);
+        if(result.hasErrors()) {    // 에러 확인
+            System.out.println("== Form data does not validated ==");
+            List<ObjectError> errors = result.getAllErrors();
+            for (ObjectError error : errors) {
+                System.out.println(error.getDefaultMessage());
+            }
+            return "createoffer";
+        }
+
+        // controller > service > dao
+        offerService.insertOffer(offer); // 에러가 없으면 데이터베이스에 저장
+        return "offercreated";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/offercreated.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: semi
+  Date: 2025. 4. 18.
+  Time: 21:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${toffer.name} 가 새로운 제안을 하였습니다. 감사합니다. <br/>
+<a href="${pageContext.request.contextPath}/offers"> Click here to view current offers </a>
+</body>
+</html>
